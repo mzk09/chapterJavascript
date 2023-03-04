834436d643a718e9fca5164506e43ec8493319b2
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2023-03-04 16:24:34 +0000

    四則演算

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ let int2 = -10;
 let float1= 3.14;
 let str1 = 'JavaScriptを覚えよう';
 
-alert(int1);
+let orange = 120;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+}else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+}else{
+  alert('みかんの値段がりんごより高い');
+}
+
