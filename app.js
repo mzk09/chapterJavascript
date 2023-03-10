66e9a87c37073bcdfcb7ab35b24af4f7574b8768
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2023-03-10 16:58:24 +0000

    四則演算

--- app.js
+++ app.js
@@ -17,3 +17,13 @@ if(orange < apple){
   alert('みかんの値段がりんごより高い');
 }
 
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num<max){
+  num = num*2;
+  count = count+1;
+}
+
+alert('2を続けて'+max+'を越えるのに必要だった回数は'+count+'回です');
