ef3cf49d4f917a9968c92c60541b2d7be0880c4f
Mizuki Morikawa <108811945+mzk09@users.noreply.github.com>
2023-03-13 15:17:53 +0000

    for文

--- app.js
+++ app.js
@@ -18,12 +18,21 @@ if(orange < apple){
 }
 
 let max = 100;
-let num = 1;
+// let num = 1;
 let count = 0;
 
-while(num<max){
-  num = num*2;
-  count = count+1;
+// while(num<max){
+//   num = num*2;
+//   count = count+1;
+// }
+
+// alert('2を続けて'+max+'を越えるのに必要だった回数は'+count+'回です');
+
+let i;
+let num=0;
+
+for(i=1;i<11;i++){
+  num = num +i;
 }
 
-alert('2を続けて'+max+'を越えるのに必要だった回数は'+count+'回です');
+alert('1から10まで足し算した結果は'+num+'です');
